b90b24e79df06a99fab4509444b2f6bff926188d
Jason Kong <jasonxkong@icloud.com>
2022-02-16 16:22:40 +0800

    added UserResults and  Useritem components

--- src/App.js
+++ src/App.js
@@ -7,26 +7,29 @@ import NotFound from './pages/NotFound';
 
 import Navbar from './components/layout/Navbar';
 import Footer from './components/layout/Footer';
+import {GithubProvider} from './context/github/GithubContext'
 
 function App() {
   return (
-    <Router>
-      <div className='flex flex-col justify-between h-screen'>
+    <GithubProvider>
+      <Router>
+        <div className='flex flex-col justify-between h-screen'>
 
-        <Navbar />
+          <Navbar />
 
-        <main className='container mx-auto px-3 pd-12'>
-          <Routes>
-            <Route path='/' element={<Home/>} />
-            <Route path='/about' element={<About/>} />
-            <Route path='/*' element={<NotFound/>} />
-          </Routes>
-        </main>
+          <main className='container mx-auto px-3 pd-12'>
+            <Routes>
+              <Route path='/' element={<Home/>} />
+              <Route path='/about' element={<About/>} />
+              <Route path='/*' element={<NotFound/>} />
+            </Routes>
+          </main>
 
-        <Footer/>
-        
-      </div>
-    </Router>
+          <Footer/>
+
+        </div>
+      </Router>
+    </GithubProvider>
   );
 }
 
